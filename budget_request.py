e25c1a7f2d7d2b4b93ece94bec75166521dd62b5
sanjaytaylor2012 <sanjaytaylor2012@gmail.com>
2023-03-28 13:39:52 -0400

    adding reset functionality

--- budget_request.py
+++ budget_request.py
@@ -4,7 +4,7 @@ import time
 
 
 # your info here
-strings = ["sup", "bro" , "dude"]
+strings = ["sup", "bro" , "dude", 'ski']
 
 current_index = 0
 
@@ -12,14 +12,22 @@ current_index = 0
 def on_hotkey_press():
     global current_index
     s = strings[current_index]
-    time.sleep(0.1)
+    # time.sleep(0.1)
     pyperclip.copy(s)
     current_index = (current_index + 1) % len(strings)
     print(f"Pasting: {s}")
     time.sleep(0.1)
 
 
+def reset():
+    global current_index
+    current_index = 0
+    print("resetting cycle")
+    time.sleep(0.1)
+
 pyperclip.copy("")
 keyboard.add_hotkey("ctrl+v", on_hotkey_press)
+keyboard.add_hotkey("ctrl+q", reset)
+
 
 keyboard.wait()
