d1609914b70d4425d31ddf9dd51515565464047c
sanjaytaylor2012 <sanjaytaylor2012@gmail.com>
2023-03-28 13:51:43 -0400

    peen

--- budget_request.py
+++ budget_request.py
@@ -12,7 +12,6 @@ current_index = 0
 def on_hotkey_press():
     global current_index
     s = strings[current_index]
-    # time.sleep(0.1)
     pyperclip.copy(s)
     current_index = (current_index + 1) % len(strings)
     print(f"Pasting: {s}")
